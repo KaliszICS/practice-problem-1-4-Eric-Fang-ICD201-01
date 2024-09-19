9223a4d2293171689f87bfe75aeb69527b5a013d
Eric-Fang-ICD201-01 <eric.fang@student.tdsb.on.ca>
2024-09-19 14:16:08 +0000

    commit

--- PP1_4.py
+++ PP1_4.py
@@ -1,16 +1,30 @@
-
+'''
+  Lesson: Input and F Strings
+  Author: Eric Fang
+  Date Created: September 19th, 2024
+  Date Last Modified: September 19th, 2024
+'''
 
 def q1():
   #Write Assignment code here
+  print(input("Input a word: "))
 
 def q2():
   #Write Assignment code here
+  print("Hello " + input("Input your first name: "))
 
 def q3():
   #Write Assignment code here
+  firstName = input("Input your first name: ")
+  lastName = input("Input your last name: ")
+  print(f"{lastName} {firstName}")
 
 def q4():
   #Write Assignment code here
+  student1 = input("Input a student: ")
+  student2 = input("Input another student")
+  print(f"Your students are {student1} and {student2}")
+
 
 #Do not edit code below this comment
 
